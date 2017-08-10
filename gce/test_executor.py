16f1b81c8d14987026c27b81beaba813db1156f5
TI\marco.orellana <marco.orellana@telusinternational.com>
2017-08-10 15:13:12 -0600

    switch log level

--- gce/test_executor.py
+++ gce/test_executor.py
@@ -40,7 +40,7 @@ home_dir = expanduser('~').replace('\\', '/')
 root_logger = logging.getLogger('cron_executor')
 root_logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stderr)
-ch.setLevel(logging.DEFAULT)
+ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
