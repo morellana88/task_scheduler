4dff79e6ee1319ecee6bb32c55ed5e5caddd6fec
TI\marco.orellana <marco.orellana@telusinternational.com>
2017-08-10 14:40:55 -0600

    refactor using pool.map

--- gce/test_executor.py
+++ gce/test_executor.py
@@ -32,20 +32,10 @@ from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool
 
 PROJECT = 'ti-ca-ml-start'  # change this to match your project
-
-TOPIC1 = 'sleep_10'
-TOPIC2 = 'sleep_20'
+cron_entries = [('sleep_10', 'gce/task1.py'), ('sleep_20', 'gce/task2.py')]
 
 # get home user directory
 home_dir = expanduser('~').replace('\\', '/')
-
-script_path1 = os.path.abspath(os.path.join(os.getcwd(), 'gce/task1.py'))
-script_path2 = os.path.abspath(os.path.join(os.getcwd(), 'gce/task2.py'))
-
-#script_path = os.path.abspath(os.path.join(os.getcwd(), 'logger_sample_task.py'))
-#sample_task = "python -u %s" % script_path
-task1 = "python -u %s" % script_path1
-task2 = "python -u %s" % script_path2
 
 root_logger = logging.getLogger('cron_executor')
 root_logger.setLevel(logging.DEBUG)
@@ -59,31 +49,19 @@ root_logger.addHandler(ch)
 cloud_handler = CloudLoggingHandler(on_gce=True, logname="task_runner")
 root_logger.addHandler(cloud_handler)
 
-# create the executor that watches the topic, and will run the job task
-# test_executor = Executor(topic=TOPIC, project=PROJECT, task_cmd=sample_task, subname='sample_task')
-executor1 = Executor(topic=TOPIC1, project=PROJECT, task_cmd=task1, subname='task1')
-executor2 = Executor(topic=TOPIC2, project=PROJECT, task_cmd=task2, subname='task2')
 
-# add a cloud logging handler and stderr logging handler
-job_cloud_handler1 = CloudLoggingHandler(on_gce=True, logname=executor1.subname)
-executor1.job_log.addHandler(job_cloud_handler1)
-executor1.job_log.addHandler(ch)
-executor1.job_log.setLevel(logging.DEBUG)
+def create_tasks((topic_name, script_path)):
+    abs_path = os.path.abspath(os.path.join(os.getcwd(), script_path))
+    task = "python -u %s" % abs_path
+    executor = [Executor(topic_name, project=PROJECT, task_cmd=task, subname='')]
+    job_cloud_handler= CloudLoggingHandler(on_gce=True, logname=executor.subname)
+    executor.job_log.addHandler(job_cloud_handler)
+    executor.job_log.addHandler(ch)
+    executor.job_log.setLevel(logging.DEBUG)
+    executor.watch_topic()
 
-# executor1.watch_topic()
-
-
-job_cloud_handler2 = CloudLoggingHandler(on_gce=True, logname=executor2.subname)
-executor2.job_log.addHandler(job_cloud_handler2)
-executor2.job_log.addHandler(ch)
-executor2.job_log.setLevel(logging.DEBUG)
-
-tasks = [executor1, executor2]
-
-# watches indefinitely
-# executor2.watch_topic()
 pool = ThreadPool(4)
-results = pool.map(Executor.watch_topic, tasks)
+results = pool.map(create_tasks, cron_entries)
 pool.close()
 pool.join()
 
