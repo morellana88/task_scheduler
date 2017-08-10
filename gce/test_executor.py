c05ddd8fbf313b01d9e2549c413b1fcb716ad96e
TI\marco.orellana <marco.orellana@telusinternational.com>
2017-08-10 10:13:39 -0600

    testing 2 watchers

--- gce/test_executor.py
+++ gce/test_executor.py
@@ -29,16 +29,22 @@ from cloud_handler import CloudLoggingHandler
 from cron_executor import Executor
 from os.path import expanduser
 
-
 PROJECT = 'ti-ca-ml-start'  # change this to match your project
-TOPIC = 'tie-rol-etl'
+
+TOPIC1 = 'sleep_10'
+TOPIC2 = 'sleep_20'
 
 # get home user directory
 home_dir = expanduser('~').replace('\\', '/')
 
-# extraction_script = "sh %s/TalendWorkspace/ATTRITION/bin/SFDC_Extraction_0.1/SFDC_Extraction/SFDC_Extraction_run.bash" % home_dir
-script_path = os.path.abspath(os.path.join(os.getcwd(), 'logger_sample_task.py'))
-sample_task = "python -u %s" % script_path
+script_path1 = os.path.abspath(os.path.join(os.getcwd(), 'task1.py'))
+script_path2 = os.path.abspath(os.path.join(os.getcwd(), 'task2.py'))
+
+#script_path = os.path.abspath(os.path.join(os.getcwd(), 'logger_sample_task.py'))
+#sample_task = "python -u %s" % script_path
+task1 = "python -u %s" % script_path1
+task2 = "python -u %s" % script_path2
+
 root_logger = logging.getLogger('cron_executor')
 root_logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stderr)
@@ -52,13 +58,21 @@ cloud_handler = CloudLoggingHandler(on_gce=True, logname="task_runner")
 root_logger.addHandler(cloud_handler)
 
 # create the executor that watches the topic, and will run the job task
-test_executor = Executor(topic=TOPIC, project=PROJECT, task_cmd=sample_task, subname='sample_task')
+# test_executor = Executor(topic=TOPIC, project=PROJECT, task_cmd=sample_task, subname='sample_task')
+executor1 = Executor(topic=TOPIC1, project=PROJECT, task_cmd=task1, subname='task1')
+executor2 = Executor(topic=TOPIC2, project=PROJECT, task_cmd=task2, subname='task2')
 
 # add a cloud logging handler and stderr logging handler
-job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=test_executor.subname)
-test_executor.job_log.addHandler(job_cloud_handler)
-test_executor.job_log.addHandler(ch)
-test_executor.job_log.setLevel(logging.DEBUG)
+job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=executor1.subname)
+executor1.job_log.addHandler(job_cloud_handler)
+executor1.job_log.addHandler(ch)
+executor1.job_log.setLevel(logging.DEBUG)
+
+
+job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=executor2.subname)
+executor2.job_log.addHandler(job_cloud_handler)
+executor2.job_log.addHandler(ch)
+executor2.job_log.setLevel(logging.DEBUG)
 
 # watches indefinitely
-test_executor.watch_topic()
+executor2.watch_topic()
