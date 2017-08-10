2bfd25b31696aff4a7b2433d5ab059c608987447
TI\marco.orellana <marco.orellana@telusinternational.com>
2017-08-10 10:48:38 -0600

    adding second watcher

--- gce/test_executor.py
+++ gce/test_executor.py
@@ -63,14 +63,16 @@ executor1 = Executor(topic=TOPIC1, project=PROJECT, task_cmd=task1, subname='tas
 executor2 = Executor(topic=TOPIC2, project=PROJECT, task_cmd=task2, subname='task2')
 
 # add a cloud logging handler and stderr logging handler
-job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=executor1.subname)
-executor1.job_log.addHandler(job_cloud_handler)
+job_cloud_handler1 = CloudLoggingHandler(on_gce=True, logname=executor1.subname)
+executor1.job_log.addHandler(job_cloud_handler1)
 executor1.job_log.addHandler(ch)
 executor1.job_log.setLevel(logging.DEBUG)
 
+executor1.watch_topic()
 
-job_cloud_handler = CloudLoggingHandler(on_gce=True, logname=executor2.subname)
-executor2.job_log.addHandler(job_cloud_handler)
+
+job_cloud_handler2 = CloudLoggingHandler(on_gce=True, logname=executor2.subname)
+executor2.job_log.addHandler(job_cloud_handler2)
 executor2.job_log.addHandler(ch)
 executor2.job_log.setLevel(logging.DEBUG)
 
